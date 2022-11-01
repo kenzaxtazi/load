13b0405e5079f43b19b72aa28b8e67f1c494e0ac
Kenza Tazi <ktazi100@yahoo.co.uk>
2022-11-01 17:52:48 +0000

    typo

--- __init__.py
+++ __init__.py
@@ -12,9 +12,9 @@ the middle of each month (float)
 
 import sys
 
-data_dir = '/Users/kenzatazi/Documents/CDT/Code'
+data_dir = '/Users/kenzatazi/Documents/CDT/Code/data'
 code_dir = '/Users/kenzatazi/Documents/CDT/Code'
- 
+
 sys.path.append(code_dir)
 
 
