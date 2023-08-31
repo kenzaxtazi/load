efc94e637aa0ef8267627dbeffb463eb200ecdbf
Kenza Tazi <ktazi100@yahoo.co.uk>
2023-08-31 13:38:21 +0100

    formatting and correct time sampling

--- aphrodite.py
+++ aphrodite.py
@@ -14,7 +14,7 @@ import load.location_sel as ls
 from load import data_dir
 
 
-def collect_APHRO(location: str or tuple, minyear: float, maxyear: float) -> xr.DataArray:
+def collect_APHRO(location: str or tuple, minyear: str, maxyear: str) -> xr.Dataset:
     """
     Download data from APHRODITE model.
 
@@ -24,55 +24,55 @@ def collect_APHRO(location: str or tuple, minyear: float, maxyear: float) -> xr.
         maxyear (float): end date in years
 
     Returns:
-        xr.DataArray: APHRODITE data
+        xr.Dataset: APHRODITE data
     """
 
-    aphro_da = xr.open_dataset(
+    aphro_ds = xr.open_dataset(
         data_dir + "APHRODITE/aphrodite_indus_1951_2016.nc")
 
     if type(location) == str:
-        loc_da = ls.select_basin(aphro_da, location)
+        loc_ds = ls.select_basin(aphro_ds, location)
     else:
         lat, lon = location
-        loc_da = aphro_da.interp(coords={"lon": lon, "lat": lat},
+        loc_ds = aphro_ds.interp(coords={"lon": lon, "lat": lat},
                                  method="nearest")
 
-    tim_da = loc_da.sel(time=slice(minyear, maxyear))
-    da = tim_da.assign_attrs(plot_legend="APHRODITE")  # in mm/day
-    return da
+    tim_ds = loc_ds.sel(time=slice(minyear, maxyear))
+    ds = tim_ds.assign_attrs(plot_legend="APHRODITE")  # in mm/day
+    return ds
 
 
 def merge_og_files():
     """Function to open, crop and merge the raw APHRODITE data files."""
 
-    da_list = []
+    ds_list = []
     extent = ls.basin_extent('indus')
 
     print('1951-2007')
     for f in tqdm(glob.glob(data_dir +
                             'APHRODITE/APHRO_MA_025deg_V1101.1951-2007.gz/*.nc')):
-        da = xr.open_dataset(f)
-        da = da.rename({'latitude': 'lat', 'longitude': 'lon', 'precip': 'tp'})
-        da_cropped = da.tp.sel(lon=slice(extent[1], extent[3]),
+        ds = xr.open_dataset(f)
+        ds = ds.rename({'latitude': 'lat', 'longitude': 'lon', 'precip': 'tp'})
+        da_cropped = ds.tp.sel(lon=slice(extent[1], extent[3]),
                                lat=slice(extent[2], extent[0]))
-        da_resampled = da_cropped.astype("datetime64[M]")
-        #da_resampled['time'] = da_resampled.time.astype(float)/365/24/60/60/1e9
-        #da_resampled['time'] = da_resampled['time'] + 1970
-        da_list.append(da_resampled)
+        ds_resampled = da_cropped.resample(time='MS').mean()
+        #ds_resampled['time'] = ds_resampled.time.astype(float)/365/24/60/60/1e9
+        #ds_resampled['time'] = ds_resampled['time'] + 1970
+        ds_list.append(ds_resampled)
 
     print('2007-2016')
     for f in tqdm(
             glob.glob(data_dir + 'APHRODITE/APHRO_MA_025deg_V1101_EXR1/*.nc')):
-        da = xr.open_dataset(f)
-        da = da.rename({'precip': 'tp'})
-        da_cropped = da.tp.sel(lon=slice(extent[1], extent[3]),
+        ds = xr.open_dataset(f)
+        ds = ds.rename({'precip': 'tp'})
+        da_cropped = ds.tp.sel(lon=slice(extent[1], extent[3]),
                                lat=slice(extent[2], extent[0]))
-        da_resampled = da_cropped.astype("datetime64[M]")
-        #da_resampled['time'] = da_resampled.time.astype(float)/365/24/60/60/1e9
-        #da_resampled['time'] = da_resampled['time'] + 1970
-        da_list.append(da_resampled)
+        ds_resampled = da_cropped.resample(time='MS').mean()
+        #ds_resampled['time'] = ds_resampled.time.astype(float)/365/24/60/60/1e9
+        #ds_resampled['time'] = ds_resampled['time'] + 1970
+        ds_list.append(ds_resampled)
 
-    da_merged = xr.merge(da_list)
+    ds_merged = xr.merge(ds_list)
 
     '''
     # Standardise time resolution
@@ -81,4 +81,4 @@ def merge_og_files():
     time_arr = np.arange(round(minyear) + 1./24., round(maxyear), 1./12.)
     da_merged['time'] = time_arr
     '''
-    da_merged.to_netcdf(data_dir + "APHRODITE/aphrodite_indus_1951_2016.nc")
+    ds_merged.to_netcdf(data_dir + "APHRODITE/aphrodite_indus_1951_2016.nc")
